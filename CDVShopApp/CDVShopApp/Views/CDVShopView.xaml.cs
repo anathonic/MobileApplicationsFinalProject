1219d51c181fc93aa04d26c7b4b44703a6c21e3c
Anita Tabaczka <anitatabacka@gmail.com>
2021-05-31 23:29:32 +0200

    start of cart creating

--- CDVShopApp/CDVShopApp/Views/CDVShopView.xaml.cs
+++ CDVShopApp/CDVShopApp/Views/CDVShopView.xaml.cs
@@ -5,11 +5,26 @@ namespace CDVShopApp.Views
 {
     public partial class CDVShopView : ContentPage
     {
+        double _pageHeight;
         public CDVShopView()
         {
             InitializeComponent();
 
             BindingContext = new CDVShopViewModel();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            _pageHeight = height;
+            CartView.TranslationY = _pageHeight - CartView.PageHeader;
+            base.OnSizeAllocated(width, height);
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
         }
     }
 }
---
+++ CDVShopApp/CDVShopApp/Views/CartView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+
+
+namespace CDVShopApp.Views
+{
+    public partial class CartView : ContentView
+    {
+        public CartView()
+        {
+            InitializeComponent();
+        }
+        public int PageHeader { get; private set; } = 70;
+    }
+}
