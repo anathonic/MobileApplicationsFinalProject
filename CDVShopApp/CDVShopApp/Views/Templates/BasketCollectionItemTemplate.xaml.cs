bb2ce57e6f9a55fdd9d10c11bd83f5dec0c7d407
Anita Tabaczka <anitatabacka@gmail.com>
2021-06-03 12:33:53 +0200

    notification button, basket continuing

---
+++ CDVShopApp/CDVShopApp/Views/Templates/BasketCollectionItemTemplate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CDVShopApp.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BasketCollectionItemTemplate : ContentView
+    {
+        public BasketCollectionItemTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- CDVShopApp/CDVShopApp/Views/Templates/BasketItemTemplateSelector.cs
+++ CDVShopApp/CDVShopApp/Views/Templates/BasketItemTemplateSelector.cs
@@ -5,12 +5,11 @@ namespace CDVShopApp.Views.Templates
     public class BasketItemDataTemplateSelector : DataTemplateSelector
     {
         public DataTemplate ProductTemplate { get; set; }
-        public DataTemplate Order { get; set; }
+        public DataTemplate OrderTemplate { get; set; }
 
         protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
         {
-            return ((BasketItem)item).BasketItemType == BasketItemType.Product ? ProductTemplate : Order;
-           
+            return ((BasketItem)item).BasketItemType == BasketItemType.Product ? ProductTemplate : OrderTemplate;
                 
         }
     }
