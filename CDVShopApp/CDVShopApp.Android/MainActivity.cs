d4ee076a3acfce8acc9282b90dd0dbf59308a5c2
Anita Tabaczka <anitatabacka@gmail.com>
2021-06-13 17:15:00 +0200

    Revert "Revert "add styles""
    This reverts commit f5fc1f149f2b74a4a8b261feb8f9cc3dc7f86052.

--- CDVShopApp/CDVShopApp.Android/MainActivity.cs
+++ CDVShopApp/CDVShopApp.Android/MainActivity.cs
@@ -7,13 +7,16 @@ using Xamarin.Forms;
 namespace CDVShopApp.Droid
 {
     [Activity(Label = "CDVShopApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
             base.OnCreate(savedInstanceState);
             Forms.SetFlags("CollectionView_Experimental");
             Forms.Init(this, savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
             LoadApplication(new App());
         }
     }
